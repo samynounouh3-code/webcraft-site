babbd12bbba2af99433203ceb722b9f1c187c099
samynounouh3-code <samy.nounouh3@gmail.com>
2026-08-03 13:18:21 +0100

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import AgenceWeb from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AgenceWeb />
-  </React.StrictMode>,
-)
