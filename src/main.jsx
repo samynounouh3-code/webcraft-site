69f33d6bd2ff6c11952e04f8f25e2ffdbc9ca3af
samynounouh3-code <samy.nounouh3@gmail.com>
2026-08-03 13:29:14 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AgenceWeb from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AgenceWeb />
+  </React.StrictMode>,
+)
